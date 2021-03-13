ddcf5c4aa2cd36badb54c0acea4e8ce2c812ca8c
Andrew <anisimov.andrew.hi@gmail.com>
2021-03-13 20:10:25 +0300

    Added model + created the transfer in presentation with thymeleaf

--- src/main/java/org/example/controllers/FirstController.java
+++ src/main/java/org/example/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam("name") String name, @RequestParam("surname") String surname) {
-        System.out.println(name + " " + surname);
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname, Model model) {
+        model.addAttribute("message", name + " " + surname);
         return "first/hello";
     }
 
